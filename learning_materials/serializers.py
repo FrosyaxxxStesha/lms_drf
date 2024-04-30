b3c13bf7b771dfd3d9c0d432361b24031b17ac0e
Tsurkan <mikh.tsurkan@gmail.com>
2024-04-30 14:28:58 +0300

    add inline serializer

--- learning_materials/serializers.py
+++ learning_materials/serializers.py
@@ -2,8 +2,15 @@ from rest_framework import serializers
 from learning_materials import models
 
 
+class LessonSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Lesson
+        fields = '__all__'
+
+
 class CourseSerializer(serializers.ModelSerializer):
     lessons_amount = serializers.SerializerMethodField()
+    lesson = LessonSerializer(many=True)
 
     class Meta:
         model = models.Course
@@ -11,9 +18,3 @@ class CourseSerializer(serializers.ModelSerializer):
 
     def get_lessons_amount(self, obj):
         return obj.lesson.count()
-
-
-class LessonSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = models.Lesson
-        fields = '__all__'

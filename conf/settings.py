31ac5178e3a490e27d4edc6d51e51c28e60bec12
Tsurkan <mikh.tsurkan@gmail.com>
2024-05-16 00:01:11 +0300

    added integration with stripe

--- conf/settings.py
+++ conf/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
+import os
 from datetime import timedelta
 from pathlib import Path
 
@@ -154,3 +155,4 @@ REST_FRAMEWORK = {
         "rest_framework_simplejwt.authentication.JWTAuthentication",
     ],
 }
+STRIPE_API_KEY = os.getenv("STRIPE_API_KEY")
